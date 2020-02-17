a1742cc7a5fd93981fc8776466fa231a984996b6
arav <aravindsapt@gmail.com>
2020-02-17 14:35:12 +0530

    Updates

---
+++ precache-manifest.4ddaadf386203aeb1fa7672f34fe577c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2049e9b7813088c006566ec956349ca",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "5375a6bbe826663758af",
+    "url": "/minesweeper/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "186dae3f5ba39dabe4b5",
+    "url": "/minesweeper/static/css/main.9c64946c.chunk.css"
+  },
+  {
+    "revision": "5375a6bbe826663758af",
+    "url": "/minesweeper/static/js/2.f4a3f1ae.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/minesweeper/static/js/2.f4a3f1ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "186dae3f5ba39dabe4b5",
+    "url": "/minesweeper/static/js/main.36ac6bfe.chunk.js"
+  },
+  {
+    "revision": "3504984439186697f6c9",
+    "url": "/minesweeper/static/js/runtime-main.ffd00b1a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/minesweeper/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/minesweeper/static/media/logo.5d5d9eef.svg"
+  }
+]);
